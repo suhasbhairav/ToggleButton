1661b06c84a164d0a891f880832c70875295c238
Suhas Bhairav <suhasbhairav@gmail.com>
2016-04-12 21:56:53 +0200

    remove space

--- app/src/main/java/main/tempcon/com/togglebutton/MainActivity.java
+++ app/src/main/java/main/tempcon/com/togglebutton/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         textView.setVisibility(View.INVISIBLE);
         aswitch = (Switch)findViewById(R.id.toogle_button_2);
         aswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
